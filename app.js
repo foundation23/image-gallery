c711c0057eb17ad4204bd4c159ef8a17cef1b5df
foundation23 <maksim.foundation@yandex.ru>
2021-11-15 17:51:57 +0300

    commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+function clidesPlagin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide')
+
+    slides[activeSlide].classList.add('active')
+    
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+    
+            slide.classList.add('active')
+        })
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
+    
+    }
+}
+clidesPlagin(2)
